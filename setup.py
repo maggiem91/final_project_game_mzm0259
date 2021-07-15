f77b2ba446e6343c4d7c213a855579bd38616b60
Maggie Maido <Maggie@Maggies-MacBook-Pro.local>
2021-07-14 22:08:27 -0400

    final commit with setup.py and readme.markdown, cleaned up code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Final_Project',
+    version='',
+    packages=['final_project_game'],
+    url='',
+    license='',
+    author='Maggie Maido',
+    author_email='maggie_maido@yahoo.com',
+    description=''
+)
