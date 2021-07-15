a016b82430b69a92075e0ef239be8df0720b9d70
Maggie <maggie_maido@yahoo.com>
2021-07-14 22:53:08 -0400

    final commit for the day

--- build/lib/final_project_game/my_game.py
+++ build/lib/final_project_game/my_game.py
@@ -17,21 +17,15 @@ class Background(Sprite):
         super().__init__(*args)
         self.image = pygame.image.load("final_project_game_images/scrolling_mushroom_background.png")
         self.image = Game.aspect_scale(self.image, int(settings.WORLD_WIDTH), int(settings.WORLD_HEIGHT))
-        # self.image = pygame.transform.scale(self.image, (settings.WORLD_WIDTH, settings.WORLD_HEIGHT))
-        # self.background = pygame.image.load("final_project_game_images/HP0083.jpg.webp")
         self.rect = self.image.get_rect()
-
-        # self.image = pygame.transform.scale(self.background, (settings.WORLD_WIDTH, settings.WORLD_HEIGHT))
         self.world_rect = self.image.get_rect().copy()
         self.world_rect.bottom = settings.WORLD_HEIGHT
-        # assert self.world_rect.width == settings.WORLD_WIDTH
 
 
 class Explosion(Sprite):
     def __init__(self, x, y, *groups):
         super().__init__(*groups)
         self.image = pygame.image.load("final_project_game_images/dead_hedgehog.png").convert_alpha()
-        # self.image.fill((0, 0, 200))
         self.world_rect = self.image.get_rect().move(x, y)
 
     def update(self, *args, **kwargs):
@@ -62,7 +56,6 @@ class Player(Sprite):
             self.move_down()
         if self.world_rect.top < 0:
             self.world_rect.top += settings.WORLD_HEIGHT
-
         if self.world_rect.top > settings.WORLD_HEIGHT:
             self.world_rect.top -= settings.WORLD_HEIGHT
 
@@ -110,26 +103,23 @@ class AngryEnemy(Sprite):
         self.world_rect = self.image.get_rect().move(x, y)
 
     def move_towards_player(self, player):
-        # Find direction vector (dx, dy) between enemy and player.
+        #find direction vector (dx, dy) between enemy and player
         dx, dy = player.rect.x - self.rect.x, player.rect.y - self.rect.y
         dist = math.hypot(dx, dy)
-        dx, dy = dx / dist, dy / dist  # Normalize.
-        # Move along this normalized vector towards the player at current speed.
+        dx, dy = dx / dist, dy / dist
+        #move along this vector towards the player at current speed
         self.rect.x += dx * self.speed
         self.rect.y += dy * self.speed
 
-    # Same thing using only pygame utilities
     def update(self, player):
         self.speed=settings.LEVEL_COUNT
-        # Find direction vector (dx, dy) between enemy and player.
+        #find direction vector (dx, dy) between enemy and player
         dirvect = pygame.math.Vector2(player.rect.x - self.rect.x,
                                       player.rect.y - self.rect.y)
         if dirvect.length() >0:
             dirvect.normalize()
-        # Move along this normalized vector towards the player at current speed.
             dirvect.scale_to_length(self.speed)
         self.world_rect.move_ip(dirvect)
-
 
 
 class Viewport:
@@ -142,10 +132,6 @@ class Viewport:
             self.left -= settings.WORLD_WIDTH
         if self.left < 0:
             self.left += settings.WORLD_WIDTH
-        # if self.left > settings.WORLD_HEIGHT:
-        #   self.player.move_ip
-        # if self.left < 0:
-        #   self.left += settings.WORLD_HEIGHT
 
     def compute_rect(self, group, dx=0):
         for sprite in group:
@@ -171,27 +157,13 @@ class Game(Sprite):
         self.cloud = Group()
         self.angry_characters = Group()
         Game.next_level(self)
-        # self.back = Background()
         self.static_sprites = Group()
         self.static_sprites.add(Background())
-        # self.static_sprites.add(self.back)
-        # self.static_sprites.add(background)
-
         self.viewport = Viewport()
         self.viewport.update(self.player)
 
     def game_loop(self):
         clock = Clock()
-
-        # game = Game(level)
-        # while Game.game_loop() is True:
-        #   print("Next Level")
-        #  level = level + 1
-        # game = Game(level)
-
-        # if level == 1 and self.player.alive() is True:
-        # while self.player.alive() is True:
-        #    Game.next_level(self)
         count = 0
         while True:
             count += 1
@@ -217,9 +189,6 @@ class Game(Sprite):
     def fade(self, width, height):
         level_image = pygame.image.load("final_project_game_images/space_bar.png")
         level_image = Game.aspect_scale(level_image, int(settings.WINDOW_WIDTH), int(settings.WINDOW_HEIGHT))
-
-        # level_image = pygame.transform.scale(level_image, (settings.WINDOW_WIDTH, settings.WINDOW_HEIGHT))
-
         self.fade = pygame.Surface((width, height))
         self.fade.fill((0, 0, 0))
         alpha = 0
@@ -229,7 +198,6 @@ class Game(Sprite):
                 alpha_vel *= -1
                 alpha += alpha_vel
                 self.fade.set_alpha(alpha)
-                # redrawWindow()
                 level_image.set_alpha(alpha)
                 self.screen.blit(level_image, (0, 0))
                 pygame.display.update()
@@ -243,9 +211,6 @@ class Game(Sprite):
         self.cloud.remove()
         self.angry_characters.remove()
 
-        # k = input("input something")
-        # if k is not None:
-        #    pass
         for i in range(level * 3):
             self.other_characters.add(OtherCharacters(random.randrange(0, settings.WORLD_WIDTH),
                                                       random.randrange(0, settings.WINDOW_HEIGHT)))
@@ -280,14 +245,12 @@ class Game(Sprite):
             self.player.kill()
             collided_with.kill()
             self.player_group.add(Explosion(self.player.world_rect.left, self.player.rect.y))
-            # self.player.rect.left, self.player.rect.top))
 
         collided_with_cloud = spritecollideany(self.player, self.cloud)
         if self.player.alive() and collided_with_cloud is not None:
             self.player.kill()
             collided_with_cloud.kill()
             self.player_group.add(Explosion(self.player.world_rect.left, self.player.rect.y))
-            # self.player_group.add(Explosion(self.player.rect.left, self.player.rect.top))
 
         collided_with_angry_characters = spritecollideany(self.player, self.angry_characters)
         if self.player.alive() and collided_with_angry_characters is not None:
@@ -297,27 +260,16 @@ class Game(Sprite):
 
     def draw(self):
         self.screen.fill((0, 0, 0))
-        # self.screen.blit(self.back.background,(0,0),self.back.background.get_rect())
-        # self.viewport.draw_group(self.static_sprites,
         self.viewport.draw_group(self.static_sprites, self.screen)
         self.viewport.draw_group(self.player_group, self.screen)
         self.viewport.draw_group(self.other_characters, self.screen)
         self.viewport.draw_group(self.cloud, self.screen)
         self.viewport.draw_group(self.angry_characters, self.screen)
 
-        # self.viewport.update_rect(self.static_sprites)
-        # self.viewport.update_rect(self.player_group)
-        # self.viewport.update_rect(self.other_characters)
-        # self.static_sprites.draw(self.screen)
-        # self.player_group.draw(self.screen)
-        # self.other_characters.draw(self.screen)
-
     def aspect_scale(img, bx, by):
-        """ Scales 'img' to fit into box bx/by.
-         This method will retain the original image's aspect ratio """
         ix, iy = img.get_size()
         if ix > iy:
-            # fit to width
+            #fit to width
             scale_factor = bx / float(ix)
             sy = scale_factor * iy
             if sy > by:
@@ -327,7 +279,7 @@ class Game(Sprite):
             else:
                 sx = bx
         else:
-            # fit to height
+            #fit to height
             scale_factor = by / float(iy)
             sx = scale_factor * ix
             if sx > bx:
@@ -337,7 +289,6 @@ class Game(Sprite):
             else:
                 sy = by
         return pygame.transform.scale(img, (int(sx), int(sy)))
-
 
 if __name__ == '__main__':
     Game().game_loop()
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Final_Project',
-    version='',
-    packages=['final_project_game'],
+    version='0.0.1',
+    packages=find_packages(),
     url='',
-    license='',
+    license='MIT',
     author='Maggie Maido',
     author_email='maggie_maido@yahoo.com',
-    description=''
+    description='Final Project',
+
 )
